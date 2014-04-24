367a163abf2207053efa36be8576bf1c5b5c05a4
Caleb Smith <caleb.smithnc@gmail.com>
2014-04-23 22:45:27 -0400

    Simplified emote plugin. Made it substitute all words in phrase

--- guile/emote.scm
+++ guile/emote.scm
@@ -1,17 +1,23 @@
+(use-modules (srfi srfi-69))
+
 (weechat:register "emote" "Caleb Smith" "0.1" "GPL3" "Emote" "" "")
 
-; Mappings of command patterns to functions and their arguments
-; Format: length, subcommand function message
-(define subcommand-patterns '(
-    ("tableflip" "(╯° °）╯︵ ┻━┻)")
-    ("lookofdisapproval" "ಠ_ಠ")
-))
+; Mappings of words with their emoticons
+(define patterns (alist->hash-table '(
+    ("tableflip" . "(╯° °）╯︵ ┻━┻)")
+    ("rageflip" . "(ノಠ益ಠ)ノ彡┻━┻")
+    ("doubleflip" . "┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻")
+    ("lookofdisapproval" . "ಠ_ಠ")
+    ("sun" . "☼")
+    ("kitaa" . "キタ━━━(゜∀゜)━━━!!!!!")
+)))
 
 
 ; Derive the tab completion string for the subcommands.
-(define subcommand-tab-completions
-    (apply string-append (map (lambda (i) (string-append "|| " i))
-        (map (lambda (i) (list-ref i 0)) subcommand-patterns))))
+(define tab-completions
+    (apply string-append
+        (map (lambda (i) (string-append "|| " i))
+            (hash-table-keys patterns))))
 
 
 (weechat:hook_command
@@ -19,10 +25,11 @@
     "/emote command"
     (string-append
         ""
-        "\nUse `/emote command`. Commands include:"
-        "\n/emote tableflip - flip"
-        "\n/emote loo - the look")
-    subcommand-tab-completions
+        "\nUse `/emote phrase`. Words in phrase will be replaced with their"
+        "\nemoticons:"
+        "\n/tableflip - flip"
+        "\n/look - the look")
+    tab-completions
     "main" "")
 
 
@@ -32,25 +39,10 @@
     weechat:WEECHAT_RC_OK)
 
 
-; Handle the IRC command given by the user
+; Handle the IRC command given by the user. Sets input buffer as a side-effect
 (define (command-handler buffer command)
-     (map (lambda (pattern)
-         (build-promise buffer command pattern)) subcommand-patterns)
-        weechat:WEECHAT_RC_OK)
-
-
-; Set users input buffer to whatever matched
-(define (insert_input buffer new-input)
-    (weechat:buffer_set buffer "input" new-input)
+    (weechat:buffer_set buffer "input"
+        (apply string-append (map (lambda (c)
+            (string-append (hash-table-ref/default patterns c c) " "))
+            (string-tokenize command))))
     weechat:WEECHAT_RC_OK)
-
-
-;; Given an inpupt `buffer`, `command` from IRC, and a subcommand `pattern`,
-;; return #f if the command does not match the pattern, otherwise return a
-;; promise that calls the pattern's function, with its corresponding arguments
-;; in the pattern
-(define (build-promise buffer command pattern)
-    (let ((input (list-ref pattern 0))
-         (arg (list-ref pattern 1)))
-        (if (string=? command input)
-            (insert_input buffer arg))))
