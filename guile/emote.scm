d8b1cabbe12da90f14b2c26d1242becb26110515
Caleb Smith <caleb.smithnc@gmail.com>
2014-05-03 13:29:47 -0400

    Added license and attribution at top of emote plugin

--- guile/emote.scm
+++ guile/emote.scm
@@ -1,6 +1,28 @@
+; Copyright (c) 2014 by csmith <caleb.smithnc@gmail.com>
+;
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;
+;
+; (this script requires WeeChat 0.4.1 or newer)
+;
+; History:
+; 2014-05-03, csmith <caleb.smithnc@gmail.com>
+;  version 0.1: initial release
+
 (use-modules (srfi srfi-69))
 
-(weechat:register "emote" "Caleb Smith" "0.1" "GPL3" "Emote" "" "")
+(weechat:register "emote" "Caleb Smith" "0.1" "GPL" "Emote" "" "")
 
 ; Mappings of words with their emoticons
 (define patterns (alist->hash-table '(
